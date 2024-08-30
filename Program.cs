8344e59c3c1fd67f49d2998b709aea1fb7be35a7
F.H. (home) <ficht@ficht.ch>
2024-08-30 12:34:16 +0200

    v2.0

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace BexioDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("######################## BEXIO DOWNLOADER ##############################");
+            Console.WriteLine("########################   Ficht Hämmerli  #############################");
+            Console.WriteLine("########################     WEPFI.CH     ##############################");
+            Console.WriteLine("####################### Kontakt: ficht@wepfi.ch ########################");
+            Console.WriteLine("########################################################################");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Bitte gib den Token ein:");
+            string token = Console.ReadLine();
+
+            string path;
+            while (true)
+            {
+                Console.WriteLine("Bitte geb den Pfad ein, wo die Dateien gespeichert werden sollen:");
+                path = Console.ReadLine();
+                // check if directory exists
+                if (Directory.Exists(path))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Der Pfad existiert nicht, soll er erstellt werden? (y/n)");
+                    string pathConfirm = Console.ReadLine();
+                    if (pathConfirm.ToLower() == "y")
+                    {
+                        Directory.CreateDirectory(path);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Was möchtest du Downloaden? Wähle:");
+            Console.WriteLine("1 - Alles (inklusive Archiv)");
+            Console.WriteLine("2 - Inbox");
+            short option;
+            string input = Console.ReadLine();
+            if (!Int16.TryParse(input, out option) || option > 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid option");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.bexio.com/3.0/files" + (option == 1 ? "?archived_state=all" : ""));
+            request.Headers.Add("Authorization", "Bearer " + token);
+            request.Headers.Add("Accept", "application/json");
+
+            string result;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+
+            List<Document> docs = JsonConvert.DeserializeObject<List<Document>>(result);
+
+            Console.WriteLine(docs.Count + " Dokumente gefunden, starte download..");
+
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Authorization", "Bearer " + token);
+
+                foreach (Document doc in docs)
+                {
+                    string filename = MakeValidFileName(doc.name + '.' + doc.extension);
+                    Console.WriteLine("Downloading " + filename);
+                    client.DownloadFile("https://api.bexio.com/3.0/files/" + doc.id + "/download", path + "/" + filename);
+                }
+            }
+
+            Console.WriteLine("Alle downloads fertig, drücke eine Taste zum beenden..");
+            Console.ReadLine();
+        }
+
+        private static string MakeValidFileName(string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
+    }
+
+    class Document
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string extension { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# Bexio-Downloader
+## Simple Konsolen Applikation zum Downloaden von Bexio Belegen
+
+Diese Anwendung ermöglicht es technischen Laien, Belege von Bexio einfach herunterzuladen. Sie ist benutzerfreundlich gestaltet und benötigt keine vorherige technische Erfahrung.
+
+## Anleitung
+
+### Vorbereitung:
+1. **Download:**
+   - Klicke oben auf den Button 'Code' und wähle die Option 'Download ZIP'.
+  
+### Installation und Ausführung:
+2. **Ausführen der Applikation:**
+   - Navigiere zum Ordner `/publish/[Betriebssystem]/` und führe `bexio-downloader` aus.
+   - Bei Windows und OSX könnte ein Warndialog erscheinen, da diese Applikation nicht zertifiziert ist.
+
+### Konfiguration und Nutzung:
+3. **Token eingeben:**
+   - Erstelle einen Token in deinen Bexio Einstellungen unter [Bexio API Tokens](https://office.bexio.com/index.php/admin/apiTokens#/).
+   - Gib den Token des gewünschten Accounts ein.
+4. **Zielordner festlegen:**
+   - Wähle den Pfad aus, wohin die Belege heruntergeladen werden sollen.
+5. **Download-Optionen wählen:**
+   - Entscheide, ob nur Belege aus der Inbox oder alle Belege inklusive Archiv heruntergeladen werden sollen.
+6. **Fertigstellung:**
+   - Die Applikation lädt die Belege herunter und speichert sie im gewählten Zielordner.
+
+## Support
+
+Für zusätzliche Optionen, Fragen oder wenn Hilfe benötigt wird, schicke bitte eine E-Mail an [ficht@wepfi.ch](mailto:ficht@wepfi.ch).
